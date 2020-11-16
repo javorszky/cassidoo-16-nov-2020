ad7e99f31948c6f64bbfd3c072102fff174bab00
Gabor Javorszky <gabor.javorszky@typeform.com>
2020-11-16 20:02:53 +0000

    Do everything in 2 loops instead of 3

--- main.go
+++ main.go
@@ -7,26 +7,15 @@ func main() {
 func specialPairs(arr []int) [][]int {
 	// create a helper map to track the indices where arr elements appear.
 	helper := make(map[int][]int)
-	for i, v := range arr {
-		helper[v] = append(helper[v], i)
-	}
 
-	// create the slice of slices
+	// create the slice of slices to keep track of the pairs.
 	pairs := make([][]int, 0)
 
-	// and then let's go over the helper to put together the pairs.
-	for _, v := range helper {
-		l := len(v)
-
-		// there's only one appearance of the number, can't make a pair out of it.
-		if l == 1 {
-			continue
-		}
-
-		// assemble the pairs.
-		for i := 0; i < l; i++ {
-			for j := i+1; j < l; j++ {
-				pairs = append(pairs, []int{v[i], v[j]})
+	for i, v := range arr {
+		helper[v] = append(helper[v], i)
+		if len(helper[v]) > 1 {
+			for _, k := range helper[v][:len(helper[v])-1] {
+				pairs = append(pairs, []int{k, i})
 			}
 		}
 	}
